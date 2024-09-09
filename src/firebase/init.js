121a6941c6c75cca96e1e89ed8b07ffd75afcccd
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2024-09-10 00:24:10 +0700

    deploy on firestore hosting

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBH450xLOrP4vyk9Zh1wNg0nTHmnorm6wk",
-  authDomain: "live-chat-be560.firebaseapp.com",
-  projectId: "live-chat-be560",
-  storageBucket: "live-chat-be560.appspot.com",
-  messagingSenderId: "627353352951",
-  appId: "1:627353352951:web:ce25b724fa3a12284f71f2",
-  measurementId: "G-XCQMKMQ77K",
+  apiKey: "AIzaSyB2dvTfPT1TmgM3bJdeb9tZjpNUoJ-koIo",
+  authDomain: "livechat-v2-48a67.firebaseapp.com",
+  projectId: "livechat-v2-48a67",
+  storageBucket: "livechat-v2-48a67.appspot.com",
+  messagingSenderId: "346478320830",
+  appId: "1:346478320830:web:6f5ebe540608e02b08d2ef",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
